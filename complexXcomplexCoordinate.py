a83e896684a8aa136ab30a602648ced8036a6836
Dennis Biber <dennisbiber88@gmail.com>
2023-10-19 00:44:53 -0400

    Equated 3=4

--- complexXcomplexCoordinate.py
+++ complexXcomplexCoordinate.py
@@ -1,3 +1,5 @@
+from sympy import E, I, pi, simplify
+
 plusX = -E**(I*pi)*I**(4)**(0.5)
 negX =  E**(I*pi)*I**(4)**(0.5)
 plusY = pi**(0.5)
@@ -6,6 +8,7 @@ plusZ = (I*pi)**(0.5)
 negZ = (-I*pi)**(0.5)
 plusW = (pi**2 + I*pi**2) **(0.5)
 negW = -(pi**2 + I*pi**2) **(0.5)
+zero = 0
 realValues = [
     plusX,
     negX,
@@ -18,39 +21,33 @@ realValues = [
     zero
 ]
 
-plusLen = 1
-negLen = -1
-plusWidth = 1
-negWidth = 1
-plusDepth = 1
-negDepth = -1
-negTime = -1
-plotList = [
-    plusLen,
-    negLen,
-    plusWidth,
-    negWidth,
-    plusDepth,
-    negDepth,
-    negTime,
+def Entanglement(a, b, c):
+    return (c**(2) + (a**(2) + b**(2))**(2))**(1/2)
 
-]
-def pyth(x, y):
-    return ((x**2 + y**2)**(0.5))
-
-x = realValues
-print(len(x))
 def thing(x):
-  return u.Entanglement(x[8], x[7], u.Entanglement(x[6], x[5], u.Entanglement(x[4], x[3], u.Entanglement(x[2], x[1], x[0]))))
+  return Entanglement(x[8], x[7], Entanglement(x[6], x[5], Entanglement(x[4], x[3], Entanglement(x[2], x[1], x[0]))))
 # def thing2(x):
 #   return u.Entanglement(x[6], x[5], u.Entanglement(x[4], x[3], u.Entanglement(x[2], x[1], x[0])))
 answer = thing(realValues)
 squareRoot2 = 2**(1/2) # 1.4142135623731
-# print(oops)
-from sympy import re, im
-def imaginaryEqualsReal(number):
-  print(type(re(number)))
-  print(im(number), I*re(number))
-  return im(number), re(number)
-print(answer)
-image0001, real0001 = imaginaryEqualsReal(answer)
+
+def BiberConjecture(x, y):
+  return (x**(answer) + (y**(answer))**(2))
+
+from cmath import tan, atan
+# when exponent is 0, number is ~1.55740...
+# when exponent is 2, number is 2.708^(302)
+# when exponent is 3, number is i
+# when exponent is 4, number is i
+# when exponent is 6, number is -1
+# when exponent is 12, number is 1
+x = simplify(tan(answer**0))
+y = simplify(tan(answer**2))
+z = simplify(tan(answer**3))
+w = simplify(tan(answer**4))
+print(x)
+print(y)
+print(z)
+print(w)
+if w == z:
+   print("True")

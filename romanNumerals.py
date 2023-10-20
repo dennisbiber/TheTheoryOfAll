5b4a8e38753773a3d0351b71ff95d013714cf868
Dennis Biber <dennisbiber88@gmail.com>
2023-10-20 12:35:18 -0400

    Solved for Real Infinity, Complex Infinity, Zero, Speed of Time, One, 3rd Derivative of Relativity

--- romanNumerals.py
+++ romanNumerals.py
@@ -11,16 +11,47 @@ m = c**(l**(x**(v**(i**o))))
 x = (m**2 + m**2) + (m**2 - m**2)
 y = (m**2 + m**2) - (m**2 - m**2)
 
-def one():
-    return (x / y)
+tF = E**-pi
+sF = I**4
+s = speedOfSpace()
+t = speedOfTime()
+sE = s**sF
+sI = s**tF
+tE = t**tF
+tI = t**sF
+
+def negHalf():
+    return E**(sE**2 + tI**2)**I
+  
+def half():
+    return E**(sE**2 - tI**2)**I
+
+def sqRt_Neg_Half():
+    return E**(tE**2 - sI**2)**I
+
+def sqRt_Pos_Half():
+    return E**(tE**2 - sI**2)**I
 
 def zero():
-    return (x - y)
+    return (E**two() + sqRt_Pos_Half()) - (E**half() + sqRt_Neg_Half())
 
-def speedOfTime():
-    return  (m**2 - m**2)**(1/2)
+def sqRt_3rd_derivative_light():
+    return sE + tE
 
-def speedOfSpace():
-    return  (m**2 + m**2)**(1/2)
-# print(c**(one() + one() + one() + one()))
-print(speedOfSpace()**(E**(pi)) + speedOfTime()**(E**(I**4)))
+def infinity_Light():
+    return sE * tE
+
+def infinity_complex():
+    return sE * tE**-(I**4)
+
+def infinity_Zero():
+    return sE * tE**-(I**2)
+
+def infinity_real():
+    return  E**sE + tE
+
+def infinity_Light():
+    return  E**sE  tE
+
+def infinity_E():
+    return
